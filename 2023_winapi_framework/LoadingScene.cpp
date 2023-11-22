321c394f6eeaccf8cb3b60b953165f729fb507c0
준이 <98876187+June060522@users.noreply.github.com>
2023-11-22 14:44:10 +0900

    gitignore

---
+++ 2023_winapi_framework/LoadingScene.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "LoadingScene.h"
+#include "SceneMgr.h"
+
+void LoadingScene::Init()
+{
+}
+
+void LoadingScene::Update()
+{
+	Scene::Update();
+}
+
+void LoadingScene::Render(HDC _dc)
+{
+	Scene::Render(_dc);
+}
+
+void LoadingScene::Release()
+{
+	Scene::Release();
+}
