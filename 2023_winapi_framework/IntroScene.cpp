d250ed8cb96565f3c94204f33ee9ca2dfab30b9b
준이 <98876187+June060522@users.noreply.github.com>
2023-11-22 14:35:16 +0900

    IntroFix

--- 2023_winapi_framework/IntroScene.cpp
+++ 2023_winapi_framework/IntroScene.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "IntroScene.h"
+#include "KeyMgr.h"
+#include "SceneMgr.h"
 
 void IntroScene::Init()
 {
@@ -7,6 +9,12 @@ void IntroScene::Init()
 
 void IntroScene::Update()
 {
+	if(KEY_DOWN(KEY_TYPE::Z))
+		SceneMgr::GetInst()->LoadScene(L"IntroScene");
+	if (KEY_DOWN(KEY_TYPE::X))
+		SceneMgr::GetInst()->LoadScene(L"StartScene");
+	if(KEY_DOWN(KEY_TYPE::C))
+		SceneMgr::GetInst()->LoadScene(L"GameScene");
 	Scene::Update();
 }
 
