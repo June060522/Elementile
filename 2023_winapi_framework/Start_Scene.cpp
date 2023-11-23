8a980038dc1cc413104ab45e51385c30de7b3529
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-11-23 21:43:08 +0900

    스타트씬

--- 2023_winapi_framework/Start_Scene.cpp
+++ 2023_winapi_framework/Start_Scene.cpp
@@ -5,6 +5,7 @@
 #include "ResMgr.h"
 void Start_Scene::Init()
 {
+
 }
 
 void Start_Scene::Update()
@@ -17,6 +18,22 @@ void Start_Scene::Update()
 void Start_Scene::Render(HDC _dc)
 {
 	Scene::Render(_dc);
+
+	HFONT hFont = CreateFont(30, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
+		CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"MerriweatherSans-ExtraBold");
+    SelectObject(_dc, hFont);
+
+    SetTextColor(_dc, RGB(255, 255, 255));
+
+    SetBkMode(_dc, TRANSPARENT);
+
+    RECT rect = { 100, 100, 500, 200 };
+
+    DrawText(_dc, L"Hello, World!", -1, &rect, DT_CENTER | DT_VCENTER);
+
+    // Clean up the font
+    DeleteObject(hFont);
+
 }
 
 void Start_Scene::Release()
