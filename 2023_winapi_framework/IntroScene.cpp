39f72f760f5b348094efc9cad80698beff9686d6
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-11-27 22:29:42 +0900

    화면애니메이션

--- 2023_winapi_framework/IntroScene.cpp
+++ 2023_winapi_framework/IntroScene.cpp
@@ -5,31 +5,71 @@
 #include "DataManager.h"
 #include <windows.h>
 #include "Button.h"
-#include "KeyMgr.h"
 #include "Core.h"
-#include "BaseWindow.h"
+#include "TimeMgr.h"
 
 int darknessLevel = 0;
-float elapsedTime = 0.0f; // 경과 시간을 나타내는 변수
-const float transitionTime = 30.0f; // 30초 후에 씬을 변경
-const int maxDarknessLevel = 100; // darknessLevel의 최대값
+float elapsedTime = -5.0f; // Set initial elapsedTime to -5 seconds
+const float darknessActivationTime = 5.0f; // Darkness activation time
+const float transitionTime = 5.0f; // 5 seconds after the scene change
+const int maxDarknessLevel = 100; // Maximum value for darknessLevel
 
 void IntroScene::Init()
 {
+    increasingDarkness = true;
+    transitionExecuted = false;
+    time = 0;
+    ftime = 100;
     DataManager::GetInst()->Init();
     AddObject(new Button(), OBJECT_GROUP::UI);
-    int result = AddFontResource(L"Res\\Font\\Font.ttf");
+    //int result = AddFontResource(L"Res\\Font\\Font.ttf");
+    int title = AddFontResource(L"Res\\Font\\인천교육소통.ttf");
+
+    elapsedTime = -5.0f;
 }
 
 void IntroScene::Update()
 {
-    float deltaTime = 1; // deltaTime을 얻는 방법을 구현 (프레임 간 시간)
-
+    float deltaTime = 1; // 실제 deltaTime을 얻기 위해 이를 대체하세요
     elapsedTime += deltaTime;
 
-    if (elapsedTime > transitionTime)
+    currentCharIndex = 0;
+    maxCharIndex =0;
+
+    if (elapsedTime >= 50.0f && darknessLevel < 350)
     {
-        darknessLevel += 1;
+        if (increasingDarkness && darknessLevel < 250)
+        {
+            darknessLevel += 1;
+        }
+        else if (!increasingDarkness && darknessLevel > 0)
+        {
+            darknessLevel = max(darknessLevel - 1, 0);
+        }
+
+        if (darknessLevel >= 250 && !transitionExecuted)
+        {
+            // darknessLevel이 250에 도달하면 천천히 하얀색으로 전환
+            float transitionDuration = 2.0f; // 전환 기간을 조절하세요
+            float transitionProgress = min((elapsedTime - darknessActivationTime) / transitionDuration, 1.0f);
+            int targetDarkness = 0; // 목표로 하는 darknessLevel
+
+            // 보간된 값으로 darknessLevel 조절
+            //darknessLevel = static_cast<int>(darknessLevel + (targetDarkness - darknessLevel) * transitionProgress);
+
+            // 전환이 완료되면 타이머와 상태를 초기화
+            if (transitionProgress >= 1.0f)
+            {
+                elapsedTime = -5.0f; // 타이머 초기화
+                increasingDarkness = true; // 전환 상태 초기화
+                transitionExecuted = true; // 전환 플래그 설정
+            }
+        }
+        else if (darknessLevel <= 0)
+        {
+            increasingDarkness = true;
+            transitionExecuted = false; // darknessLevel이 250 아래로 내려가면 전환 플래그 초기화
+        }
     }
 
     if (KEY_DOWN(KEY_TYPE::Z))
@@ -37,11 +77,7 @@ void IntroScene::Update()
 
     if (KEY_DOWN(KEY_TYPE::X))
     {
-        // darknessLevel이 일정 수치에 도달하면 StartScene으로 전환
-        if (darknessLevel >= 250)
-        {
-            SceneMgr::GetInst()->LoadScene(L"StartScene");
-        }
+        SceneMgr::GetInst()->LoadScene(L"StartScene");
     }
 
     if (KEY_DOWN(KEY_TYPE::C))
@@ -57,8 +93,7 @@ void IntroScene::Render(HDC _dc)
     RECT rcClient;
     GetClientRect(Core::GetInst()->GetHwnd(), &rcClient);
 
-    // darknessLevel이 255를 초과하지 않도록 조절
-    darknessLevel = min(darknessLevel, 255);
+    darknessLevel = min(max(darknessLevel, 0), 255);
 
     COLORREF bgColor = RGB(255 - darknessLevel, 255 - darknessLevel, 255 - darknessLevel);
     HBRUSH hBrush = CreateSolidBrush(bgColor);
@@ -66,13 +101,21 @@ void IntroScene::Render(HDC _dc)
     DeleteObject(hBrush);
 
     HFONT hFont = CreateFont(200, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
-        CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Merriweather Sans ExtraBold");
+        CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"인천교육소통");
     SelectObject(_dc, hFont);
 
-    TextOut(_dc, pos.x, pos.y, L"NoBland", wcslen(L"NoBland"));
-    SetTextColor(_dc, RGB(255, 0, 0));
-
-    SetBkMode(_dc, TRANSPARENT);
+    if (darknessLevel >= 250)
+    {
+        SetTextColor(_dc, RGB(255, 0, 0));
+        SetBkMode(_dc, TRANSPARENT);
+        TextOut(_dc, pos.x, pos.y, L"June and min", wcslen(L"June and min"));
+    }
+    else
+    {
+        SetTextColor(_dc, RGB(255, 255, 255));
+        SetBkMode(_dc, TRANSPARENT);
+        TextOut(_dc, pos.x, pos.y, L"난쟁2", wcslen(L"난쟁2"));
+    }
 
     DeleteObject(hFont);
 }
@@ -80,5 +123,6 @@ void IntroScene::Render(HDC _dc)
 void IntroScene::Release()
 {
     Scene::Release();
-    RemoveFontResource(L"Res\\Font\\Font.ttf");
+    //RemoveFontResource(L"Res\\Font\\Font.ttf");
+    RemoveFontResource(L"Res\\Font\\인천교육소통.ttf");
 }
--- 2023_winapi_framework/IntroScene.h
+++ 2023_winapi_framework/IntroScene.h
@@ -6,9 +6,16 @@ class IntroScene :
 public:
     virtual void Init() override;
     virtual void Update() override;
+    float lerp(float a, float b, float t);
     virtual void Render(HDC _dc) override;
     virtual void Release() override;
 private:
     Vec2 pos = Vec2(500, 350);
+     bool increasingDarkness;
+     bool transitionExecuted;
+     float time;
+     float ftime;
+     int currentCharIndex;
+     int maxCharIndex;
 };
 
