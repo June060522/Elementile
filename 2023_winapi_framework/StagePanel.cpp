63b5240870eaa3023fe98c9332c517ed9b5a3a06
준이 <98876187+June060522@users.noreply.github.com>
2023-12-12 00:54:38 +0900

    Fade

--- 2023_winapi_framework/StagePanel.cpp
+++ 2023_winapi_framework/StagePanel.cpp
@@ -35,16 +35,32 @@ void StagePanel::Update()
 
 void StagePanel::Render(HDC _dc)
 {
+	BLENDFUNCTION blendFunc;
+	blendFunc.BlendOp = AC_SRC_OVER;
+	blendFunc.BlendFlags = 0;
+	blendFunc.SourceConstantAlpha = 128;
+	blendFunc.AlphaFormat = AC_SRC_ALPHA;
+
 	float width = m_pDarkBG->GetWidth();
 	float height = m_pDarkBG->GetHeight();
-	Vec2 vScale = {200, 200};
+	Vec2 vScale = { 200, 200 };
 	Vec2 vPos = { (int)Core::GetInst()->GetResolution().x / 2, (int)Core::GetInst()->GetResolution().y / 2 };
 	float left = vPos.x - (width * (vScale.x / 200.f));
 	float top = vPos.y - (height * (vScale.y / 200.f));
-	TransparentBlt(_dc, left, top,
-		width * (vScale.x / 100.f), height * (vScale.y / 100.f),
-		m_pDarkBG->GetDC(), 0, 0,
-		width, height, RGB(255, 0, 255));
+
+	AlphaBlend(
+		_dc,     // 대상 DC
+		left,       // 대상 DC의 X 좌표
+		top,       // 대상 DC의 Y 좌표
+		width * (vScale.x / 100.f),   // 대상 DC의 폭
+		height * (vScale.y / 100.f),  // 대상 DC의 높이
+		m_pDarkBG->GetDC(),
+		0,
+		0,
+		width,
+		height,
+		blendFunc    // 블렌딩 옵션
+	);
 
 	width = m_pBack->GetWidth();
 	height = m_pBack->GetHeight();
