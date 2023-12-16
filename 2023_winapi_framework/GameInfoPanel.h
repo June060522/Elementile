785d37ae7675125008349c5eab6bfec5bd8342d6
준이 <98876187+June060522@users.noreply.github.com>
2023-12-16 16:12:36 +0900

    merge

--- 2023_winapi_framework/GameInfoPanel.h
+++ 2023_winapi_framework/GameInfoPanel.h
@@ -13,12 +13,26 @@ public:
 private:
 	void ExplainRender(HDC _dc);
 	void BGRender(HDC _dc);
+	void TexRender(HDC _dc);
 	void BGUpdate();
 
 private:
-	
 	GameSceneUI* m_pGameSceneUI;
 	Texture* m_pDarkBG;
 	Texture* m_pBack;
-};
 
+	Texture* m_pFire;
+	Texture* m_pWater;
+	Texture* m_pGrass;
+	Texture* m_pWind;
+	Texture* m_pArrowRU;
+	Texture* m_pArrowRD;
+	Texture* m_pArrowL;
+	Texture* m_pArrowR;
+	Texture* m_pLockF;
+	Texture* m_pLockW;
+	Texture* m_pLockG;
+	Texture* m_pPlus;
+	Texture* m_pMinus;
+	Texture* m_pTeleport;
+};
