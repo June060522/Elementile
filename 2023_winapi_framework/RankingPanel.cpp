8fc5619f4e4639e8f5cad8a93ff359c8ff1b9fb1
준이 <98876187+June060522@users.noreply.github.com>
2023-12-16 09:37:21 +0900

    패널 및 서버 저장

--- 2023_winapi_framework/RankingPanel.cpp
+++ 2023_winapi_framework/RankingPanel.cpp
@@ -79,19 +79,22 @@ void RankingPanel::BackIconRender(HDC _dc)
 
 void RankingPanel::RankingRender(HDC _dc)
 {
+	HFONT hFont = CreateFont(50, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
+		CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"ï¿½ï¿½Ãµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½");
+	SelectObject(_dc, hFont);
 	{
 		Vec2 vPos = {725, 300};
 		Vec2 vScale = {35,35};
 		int Width = m_pFirstPlace->GetWidth();
 		int Height = m_pFirstPlace->GetHeight();
 
-		float centerX = vPos.x + 73;
-		float centerY = vPos.y + 80;
+		float centerX = vPos.x;
+		float centerY = vPos.y;
 
 		float left = centerX - (Width * (vScale.x / 200));
 		float top = centerY - (Height * (vScale.y / 200));
-		wstring id = L"ÀÌ¸§ : ";
-		wstring score = L"½ºÅ×ÀÌÁö : ";
+		wstring id = L"ì´ë¦„ : ";
+		wstring score = L"ìŠ¤í…Œì´ì§€ : ";
 		id += m_pGameSceneUI->GetId(0);
 		score += to_wstring(m_pGameSceneUI->GetScore(0));
 		TransparentBlt(_dc, left, top + 5,
@@ -99,68 +102,69 @@ void RankingPanel::RankingRender(HDC _dc)
 			m_pFirstPlace->GetDC(), 0, 0,
 			Width, Height, RGB(255, 0, 255));
 
-		TextOut(_dc, vPos.x, vPos.y + 200,
+		TextOut(_dc, vPos.x - 80, vPos.y + 100,
 			id.c_str(),
 			id.size());
-		TextOut(_dc, vPos.x, vPos.y + 250,
+		TextOut(_dc, vPos.x- 80, vPos.y + 150,
 			score.c_str(),
 			score.size());
 	}
 
 	{
-		Vec2 vPos = { 725, 300 };
-		Vec2 vScale = { 35,35 };
-		int Width = m_pFirstPlace->GetWidth();
-		int Height = m_pFirstPlace->GetHeight();
+		Vec2 vPos = { 425, 400 };
+		Vec2 vScale = { 30,30 };
+		int Width = m_pSecondPlace->GetWidth();
+		int Height = m_pSecondPlace->GetHeight();
 
-		float centerX = vPos.x + 73;
-		float centerY = vPos.y + 80;
+		float centerX = vPos.x;
+		float centerY = vPos.y;
 
 		float left = centerX - (Width * (vScale.x / 200));
 		float top = centerY - (Height * (vScale.y / 200));
-		wstring id = L"ÀÌ¸§ : ";
-		wstring score = L"½ºÅ×ÀÌÁö : ";
-		id += m_pGameSceneUI->GetId(0);
-		score += to_wstring(m_pGameSceneUI->GetScore(0));
+		wstring id = L"ì´ë¦„ : ";
+		wstring score = L"ìŠ¤í…Œì´ì§€ : ";
+		id += m_pGameSceneUI->GetId(1);
+		score += to_wstring(m_pGameSceneUI->GetScore(1));
 		TransparentBlt(_dc, left, top + 5,
 			Width * (vScale.x / 100), Height * (vScale.y / 100),
-			m_pFirstPlace->GetDC(), 0, 0,
+			m_pSecondPlace->GetDC(), 0, 0,
 			Width, Height, RGB(255, 0, 255));
 
-		TextOut(_dc, vPos.x, vPos.y + 200,
+		TextOut(_dc, vPos.x - 80, vPos.y + 100,
 			id.c_str(),
 			id.size());
-		TextOut(_dc, vPos.x, vPos.y + 250,
+		TextOut(_dc, vPos.x - 80, vPos.y + 150,
 			score.c_str(),
 			score.size());
 	}
 
 
 	{
-		Vec2 vPos = { 725, 300 };
-		Vec2 vScale = { 35,35 };
-		int Width = m_pFirstPlace->GetWidth();
-		int Height = m_pFirstPlace->GetHeight();
+		Vec2 vPos = { 1025, 400 };
+		Vec2 vScale = { 25,25 };
+		int Width = m_pThirdPlace->GetWidth();
+		int Height = m_pThirdPlace->GetHeight();
 
-		float centerX = vPos.x + 73;
-		float centerY = vPos.y + 80;
+		float centerX = vPos.x;
+		float centerY = vPos.y;
 
 		float left = centerX - (Width * (vScale.x / 200));
 		float top = centerY - (Height * (vScale.y / 200));
-		wstring id = L"ÀÌ¸§ : ";
-		wstring score = L"½ºÅ×ÀÌÁö : ";
-		id += m_pGameSceneUI->GetId(0);
-		score += to_wstring(m_pGameSceneUI->GetScore(0));
+		wstring id = L"ì´ë¦„ : ";
+		wstring score = L"ìŠ¤í…Œì´ì§€ : ";
+		id += m_pGameSceneUI->GetId(2);
+		score += to_wstring(m_pGameSceneUI->GetScore(2));
 		TransparentBlt(_dc, left, top + 5,
 			Width * (vScale.x / 100), Height * (vScale.y / 100),
-			m_pFirstPlace->GetDC(), 0, 0,
+			m_pThirdPlace->GetDC(), 0, 0,
 			Width, Height, RGB(255, 0, 255));
 
-		TextOut(_dc, vPos.x, vPos.y + 200,
+		TextOut(_dc, vPos.x - 80, vPos.y + 100,
 			id.c_str(),
 			id.size());
-		TextOut(_dc, vPos.x, vPos.y + 250,
+		TextOut(_dc, vPos.x - 80, vPos.y + 150,
 			score.c_str(),
 			score.size());
 	}
+	DeleteObject(hFont);
 }
